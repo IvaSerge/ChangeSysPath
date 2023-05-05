1ad23471c64812a8a0b53d695e7a1db9084c2784
IvaSerge <ivaserge@ukr.net>
2023-05-05 08:48:00 +0200

    check_circuit

--- path_is_custom/path_is_custom.py
+++ path_is_custom/path_is_custom.py
@@ -32,6 +32,17 @@ reload(toolsrvt)
 from toolsrvt import *
 
 
+def check_circuit(_circuit):
+	is_data_circuit = _circuit.SystemType == Electrical.ElectricalSystemType.PowerCircuit
+	is_elec_circuit = _circuit.SystemType == Electrical.ElectricalSystemType.Data
+	if not any([is_data_circuit, is_elec_circuit]):
+		return None
+	circuit_mode_custom = _circuit.CircuitPathMode == Autodesk.Revit.DB.Electrical.ElectricalCircuitPathMode.Custom
+	if not circuit_mode_custom:
+		return _circuit
+	return None
+
+
 # ================ GLOBAL VARIABLES
 doc = DocumentManager.Instance.CurrentDBDocument
 uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
@@ -39,17 +50,18 @@ uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 view = doc.ActiveView
 
-# reload_IN = IN[1]  # type: ignore
-# csv_data = dir_path + "\\" + IN[2]  # type: ignore
-# outlist = list()
+reload_IN = IN[1]  # type: ignore
+csv_data = dir_path + "\\" + IN[2]  # type: ignore
+outlist = list()
+circuits = FilteredElementCollector(doc).\
+	OfCategory(BuiltInCategory.OST_ElectricalCircuit).\
+	WhereElementIsNotElementType().\
+	ToElements()
 
-# # ================ get all circuits
-# cbreakers_list = csvreader.get_breakers_info(csv_breakers)
-# cb_paramts_to_set = csvreader.csv_to_rvt_elements(cbreakers_list, doc)
+for circuit in circuits:
+	check = check_circuit(circuit)
+	if not check:
+		continue
+	outlist.append(check)
 
-# # ================ get circuit info
-
-# # ================ write to csv
-
-
-OUT = None
+OUT = outlist
